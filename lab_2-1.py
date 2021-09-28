b9e4ba73c05afdb5bf948d38916207f2994b1067
p22strautz <p22strautz@fairfieldprep.org>
2021-09-28 10:04:32 -0400

    Cycle 3 Day 2 Labs

---
+++ lab_2-1.py
@@ -0,0 +1,6 @@
+# Author: SCT (ADMG) 9/28/21
+
+num = input("Type in a number:\n")
+
+if int(num) % 2 == 0:
+    print(num + " is a even number")
---
+++ lab_2-2.py
@@ -0,0 +1,8 @@
+# Author: SCT (ADMG) 9/28/21
+
+num = input("Type in a number:\n")
+
+if int(num) % 2 == 0:
+    print(num + " is a even number.")
+else:
+    print(num + " is a odd number.")
