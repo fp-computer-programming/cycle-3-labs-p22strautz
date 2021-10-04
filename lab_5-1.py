99f02f9cb96ce89ad077762fa3a0d2718c2d7831
p22strautz <p22strautz@fairfieldprep.org>
2021-10-04 09:56:51 -0400

    lab 5-1

---
+++ lab_5-1.py
@@ -0,0 +1,16 @@
+# Author: SCT (ADMG) 10/4/21
+
+# int(a)
+# Name Error
+
+# int('a')
+# Value Error
+
+# 'a'+2
+# Type Error
+
+# import date
+# Module Not Found Error
+
+# print("I'm a happy camper!)
+# Syntax Error
